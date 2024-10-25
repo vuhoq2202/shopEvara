6ef96e87c6014025d051e2e947848cebcdec99f9
vuhoq2202 <justhuongnef@gmail.com>
2024-10-25 11:56:26 +0700

    update-project

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,21 @@ var swiperCategories = new Swiper(".categories__container", {
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[content]');
+
+  tabs.forEach((tab) =>{
+    tab.addEventListener('click', () => {
+      const target = document.querySelector(tab.dataset.target);
+      tabContents.forEach((tabContent) =>{
+        tabContent.classList.remove('active-tab');
+      });
+
+      target.classList.add('active-tab');
+
+      tabs.forEach((tab) =>{
+        tab.classList.remove('active-tab');
+      });
+    });
+
+  });
