9aa4939bd2f071f67797035b64e19637f2ecfbec
vuhoq2202 <justhuongnef@gmail.com>
2024-10-25 11:03:25 +0700

    project

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== Menu Show =====*/
+/* Validate if constant exists */
+
+/*===== Hide Show =====*/
+/* Validate if constant exists */
+
+/*=============== IMAGE GALLERY ===============*/
+
+/*=============== SWIPER CATEGORIES ===============*/
+var swiperCategories = new Swiper(".categories__container", {
+    spaceBetween: 24,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 5,
+          spaceBetween: 24,
+        },
+      },
+  });
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+/*=============== PRODUCTS TABS ===============*/
